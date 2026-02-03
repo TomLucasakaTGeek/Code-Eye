3803105024b9a61a2c880ce8b26c100ad94b09ce
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2026-02-03 23:26:41 +0530

    Updating README and updating AI response

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,9 +1,18 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai")
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY)
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" })
+const model = genAI.getGenerativeModel({ 
+    model: "gemini-2.0-flash",
+    systemInstruction: `
+        You are a code reviewer, who have an expertise in development reviewing a codebase, you look for the code and find the problems and suggest the solution to the developer.
 
-const prompt = "Explain how AI works"
+        ${code}
+    `
+})
 
-const result = await model.generateContent(prompt)
-console.log(result.response.text())
+async function generateContent(prompt) {
+    const result = await model.generateContent(prompt)
+    return result.response.text()
+}
+
+module.exports = generateContent
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Code-Eye
-An automated code reviewer
+An automated code reviewer to check problems with your code and suggest possible fix
+
+# Tech-Stack
+- React.js
+- Node.js
+- Gemini API
