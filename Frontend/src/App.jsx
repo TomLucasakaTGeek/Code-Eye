369cbe5c6ab0a9b65c61cfbe7de498a93a8707cb
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2026-02-04 08:12:14 +0530

    Updating Frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,31 +1,54 @@
 import { useEffect, useState } from 'react'
 import "prismjs/themes/prism-tomorrow.css"
 import prism from "prismjs"
+import Markdown from 'react-markdown'
+import axios from 'axios'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [code, setCode] = useState(`function sum() {
+        return 1 + 1
+      }`)
+
+  const [ review, setReview ] = useState(``)
 
   useEffect(()=>{
     prism.highlightAll()
-  })
+  }, [])
+
+  async function reviewCode(params) {
+    const res = await axios.post('http://localhost:3000/ai/get-review', { code })
+    setReview(res.data)
+  }
 
   return (
     <>
       <main>
         <div className='left'>
           <div className='code'>
-            <pre>
-              <code className='language-javascript'>
-                {` function sum() {
-                      return 1 + 1
-                    }`}
-              </code>
-            </pre>
+            <Editor
+              value={code}
+              onValueChange={code => setCode(code)}
+              highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+              padding={10}
+              style={{
+                fontFamily: ' "Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%" 
+              }}
+            />
           </div>
-          <div className='review'>Review</div>
+          <div className='review'
+            onClick={reviewCode}
+          >Review</div>
         </div>
-        <div className='right'></div>
+        <div className='right'>
+            <Markdown>{review}</Markdown>
+        </div>
       </main>
     </>
   )
